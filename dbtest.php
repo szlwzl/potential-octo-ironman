4c9b58629a11a6f0ea379bcb4afb1dfca08a0b8e
Simon Whittaker <simon@verticalstructure.com>
2014-02-28 23:55:09 +0000

    adding

---
+++ dbtest.php
@@ -0,0 +1,17 @@
+$db = new mysqli('54.246.36.3', 'scalr', 'nmK4WFmDID8C7cEmcAMj', 'demo');
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+$sql = <<<SQL
+    SELECT *
+    FROM `users`
+    WHERE `live` = 1 
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+while($row = $result->fetch_assoc()){
+    echo $row['username'] . '<br />';
+}
